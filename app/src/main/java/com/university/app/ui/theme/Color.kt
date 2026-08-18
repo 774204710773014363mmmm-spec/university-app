c489008f034d2a4d1f41457cc07ed727d0cad0aa
774204710773014363mmmm-spec <774204710773014363mmmm@gmail.com>
2026-08-18 20:21:38 +0300

    update app/src/main/java/com/university/app/ui/theme/Color.kt

---
+++ app/src/main/java/com/university/app/ui/theme/Color.kt
@@ -0,0 +1,22 @@
+package com.university.app.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Navy = Color(0xFF1E3A8A)
+val NavyDark = Color(0xFF172F6E)
+val NavyLight = Color(0xFF3B5BA5)
+val NavyContainer = Color(0xFFE8EDF7)
+val OnNavyContainer = Color(0xFF1E3A8A)
+
+val Background = Color(0xFFF7F9FC)
+val SurfaceWhite = Color(0xFFFFFFFF)
+val DividerGray = Color(0xFFE5E9F0)
+
+val TextPrimary = Color(0xFF1F2937)
+val TextSecondary = Color(0xFF6B7280)
+
+val PresentGreen = Color(0xFF16A34A)
+val PresentGreenContainer = Color(0xFFE7F6EC)
+val AbsentRed = Color(0xFFDC2626)
+val AbsentRedContainer = Color(0xFFFBEAEA)
+val WarningAmber = Color(0xFFD97706)
